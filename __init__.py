39627a0d705029f0182e930c0a1257a48416c2c2
Euni14 <104272751+Euni14@users.noreply.github.com>
2024-02-03 14:49:19 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -55,6 +55,18 @@ def Readfiche(post_id):
     
     # Rendre le template HTML et transmettre les données
     return render_template('read_data.html', data=data)
+
+@app.route('/recheche_fiche_client/<string:name>')
+def Readfiche(post_id):
+    conn = sqlite3.connect('database.db')
+    cursor = conn.cursor()
+    param = "%name%"
+    cursor.execute('SELECT * FROM clients WHERE name LIKE ? ', (name,))
+    data = cursor.fetchall()
+    conn.close()
+    
+    # Rendre le template HTML et transmettre les données
+    return render_template('read_data.html', data=data)
                                                                                                                                        
 if __name__ == "__main__":
   app.run(debug=True)
