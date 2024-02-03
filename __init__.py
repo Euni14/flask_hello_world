3dc982ff31e2904517d4672b15466d38bd1a3d4f
Euni14 <104272751+Euni14@users.noreply.github.com>
2024-02-03 16:07:06 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template_string, render_template, jsonify
+from flask import Flask, render_template_string, render_template, jsonify, request
 from flask import render_template
 from flask import json
 from urllib.request import urlopen
@@ -81,7 +81,14 @@ def enregistrer_client():
     prenom = request.form['prenom']
     adresse = request.form['adresse']
 
-   
+    # Connexion à la base de données
+    conn = sqlite3.connect('database.db')
+    cursor = conn.cursor()
+
+    # Exécution de la requête SQL pour insérer un nouveau client
+    cursor.execute('INSERT INTO clients (nom, prenom, adresse) VALUES (?, ?, ?)', (nom, prenom, adresse))
+    conn.commit()
+    conn.close()
 
     return redirect(url_for('/consulation/'))  # Rediriger vers la page d'accueil après l'enregistrement
 
