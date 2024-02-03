bb336b2812e362b997f17cc2f6757a8104a6c190
Euni14 <104272751+Euni14@users.noreply.github.com>
2024-02-03 16:20:09 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -77,7 +77,20 @@ def formulaire_client():
 
 @app.route('/enregistrer_client', methods=['POST'])
 def enregistrer_client():
-    return render_template('formulaire.html')
+    nom = request.form['nom']
+    prenom = request.form['prenom']
+    adresse = request.form['adresse']
+
+    # Connexion à la base de données
+    conn = sqlite3.connect('database.db')
+    cursor = conn.cursor()
+
+    # Exécution de la requête SQL pour insérer un nouveau client
+    cursor.execute('INSERT INTO clients (nom, prenom, adresse) VALUES (?, ?, ?)', (nom, prenom, adresse))
+    conn.commit()
+    conn.close()
+
+    #return redirect(url_for('/consulation/'))  # Rediriger vers la page d'accueil après l'enregistrement
 
                                                                                                                                        
 if __name__ == "__main__":
